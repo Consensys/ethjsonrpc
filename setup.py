10be852705221f928ed1e1070395283cf0722788
maurycyp <github.com@wayheavy.com>
2015-09-04 17:23:36 -0700

    Add setup.py and LICENSE files

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='ethjsonrpc',
+    version='0.1.0',
+    description='Ethereum JSON-RPC client',
+    long_description='Ethereum JSON-RPC client',
+    author='ConsenSys',
+    author_email=['info@consensys.net'],
+    url='https://github.com/ConsenSys/ethjsonrpc',
+    packages=['ethjsonrpc'],
+    license='Unlicense',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+    install_requires=[
+        'ethereum==1.0.0',
+        'ethereum-serpent==2.0.0',
+        'requests==2.7.0',
+    ],
+)
