f66a907152c2ace89180c751ffbff2afd5d8ddf9
maurycyp <github.com@wayheavy.com>
2015-09-13 18:15:26 -0700

    Add test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+from ethjsonrpc import EthJsonRpc
+
+methods = [
+    'web3_clientVersion',
+    'net_version',
+    'net_listening',
+    'net_peerCount',
+    'eth_protocolVersion',
+    'eth_coinbase',
+    'eth_mining',
+    'eth_hashrate',
+    'eth_gasPrice',
+    'eth_accounts',
+    'eth_blockNumber',
+    'eth_getCompilers',
+    'eth_newPendingTransactionFilter',
+    'eth_getWork',
+#    'shh_version',
+#    'shh_newIdentity',
+#    'shh_newGroup',
+]
+
+c = EthJsonRpc()
+print len(methods)
+for m in methods:
+    meth = getattr(c, m)
+    result = meth()
+    print '%s: %s (%s)' % (m, result, type(result))
