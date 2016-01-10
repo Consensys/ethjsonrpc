28f868578d5128239cd16ecf729d4ac6deff68d3
maurycyp <github.com@wayheavy.com>
2016-01-10 14:35:52 -0800

    Bump version to 0.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='ethjsonrpc',
-    version='0.2.4',
+    version='0.2.5',
     description='Ethereum JSON-RPC client',
     long_description=open('README.rst').read(),
     author='ConsenSys',
