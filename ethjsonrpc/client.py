5402d56ac9601e544fdfaa2263badc93831c539f
maurycyp <github.com@wayheavy.com>
2016-07-19 17:50:43 -0700

    Add PARITY_DEFAULT_RPC_PORT to client.py

--- ethjsonrpc/client.py
+++ ethjsonrpc/client.py
@@ -13,6 +13,7 @@ from ethjsonrpc.exceptions import (ConnectionError, BadStatusCodeError,
 
 GETH_DEFAULT_RPC_PORT = 8545
 ETH_DEFAULT_RPC_PORT = 8545
+PARITY_DEFAULT_RPC_PORT = 8080
 PYETHAPP_DEFAULT_RPC_PORT = 4000
 
 
